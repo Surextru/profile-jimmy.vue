43c0a321f214a1038b5982de6bb02545663fd2df
Surextru <jaime.luensan@gmail.com>
2021-09-21 18:41:50 +0200

    data.js (not definitive) - setup components - about me comp

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <h2>{{ position }}</h2>
+  </div>
+</template>
+
+<script>
+import {aboutme} from "../data"
+export default {
+  data() {
+    return {
+      name: aboutme.name,
+      position: aboutme.position
+
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/data.js
@@ -0,0 +1,178 @@
+export const aboutme = {
+    name: "jaime",
+    position: "web developer",
+    image: ""
+};
+export const skills = [
+  {
+    type: "hardskills",
+    skills: [
+      {
+        id: 1,
+        skill: "html",
+        level: 5,
+      },
+      {
+        id: 2,
+        skill: "css",
+        level: 5,
+      },
+      {
+        id: 3,
+        skill: "javascript",
+        level: 5,
+      },
+      {
+        id: 4,
+        skill: "react",
+        level: 5,
+      },
+      {
+        id: 5,
+        skill: "vue",
+        level: 5,
+      },
+      {
+        id: 6,
+        skill: "java",
+        level: 5,
+      },
+      {
+        id: 7,
+        skill: "python",
+        level: 5,
+      },
+      {
+        id: 8,
+        skill: "django",
+        level: 5,
+      },
+      {
+        id: 9,
+        skill: "MySQL",
+        level: 5,
+      },
+      {
+        id: 10,
+        skill: "SQLDeveloper",
+        level: 5,
+      },
+      {
+        id: 11,
+        skill: "git/github",
+        level: 5,
+      },
+      {
+        id: 12,
+        skill: "PHP",
+        level: 5,
+      },
+      {
+        id: 13,
+        skill: "SQLDeveloper",
+        level: 5,
+      },
+    ],
+  },
+  {
+    type: "softskills",
+    skills: [
+      { label: "autodidacta", description: "blablabla" },
+      { label: "autodidacta", description: "blablabla" },
+      { label: "autodidacta", description: "blablabla" },
+      { label: "autodidacta", description: "blablabla" },
+    ],
+  },
+];
+export const languages = [
+  {
+    id: 1,
+    language: "spanish",
+    level: "native",
+  },
+  {
+    id: 2,
+    language: "english",
+    level: "medium-high",
+  },
+  {
+    id: 3,
+    language: "catalan",
+    level: "high",
+  },
+];
+export const experties = {
+  company: [
+    {
+      position: "web developer",
+      dateIn: "",
+      dateOut: "",
+      company: "Syncrotron Alba",
+      function: ["", ""],
+    },
+  ],
+  projects: [
+    {
+      id: 1,
+      projects_name: "cocktailmaker",
+      description: "blabla",
+      repository: "https://github",
+      link_page: "https://netlify.com",
+      tecnolgies: ["", "", "", ""],
+    },
+    {
+      id: 2,
+      projects_name: "",
+      description: "blabla",
+      repository: "https://github",
+      link_page: "https://netlify.com",
+      tecnolgies: ["", "", "", ""],
+    },
+    {
+      id: 3,
+
+      projects_name: "",
+      description: "blabla",
+      repository: "https://github",
+      link_page: "https://netlify.com",
+      tecnolgies: ["", "", "", ""],
+    },
+    {
+      id: 4,
+
+      projects_name: "",
+      description: "blabla",
+      repository: "https://github",
+      link_page: "https://netlify.com",
+      tecnolgies: ["", "", "", ""],
+    },
+    {
+      id: 5,
+      projects_name: "",
+      description: "blabla",
+      repository: "https://github",
+      link_page: "https://netlify.com",
+      tecnolgies: ["", "", "", ""],
+    },
+  ],
+};
+
+export const formation = {
+  regular: [
+    {
+      study: "",
+      center: "",
+      dateIn: "",
+      dateOut: "",
+      learned: [],
+    },
+    {
+      study: "",
+      center: "",
+      dateIn: "",
+      dateOut: "",
+      learned: [],
+    },
+  ],
+  self_taught: [],
+};
